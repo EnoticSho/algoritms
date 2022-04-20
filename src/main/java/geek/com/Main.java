7e89a54f64153d22cbafaaf6a82ed95d74e08558
sergey <paradise666hell@mail.ru>
2022-04-20 11:37:43 +0300

    Возведение числа в степень(рекурсия)

--- src/main/java/geek/com/Main.java
+++ src/main/java/geek/com/Main.java
@@ -1,4 +1,16 @@
 package geek.com;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(power(2,3));
+        System.out.println(power(2,10));
+        System.out.println(power(5,3));
+    }
+
+    public static long power(long number, int p){
+        if (p == 0){
+            return 1;
+        }
+        return number*power(number, --p);
+    }
 }
